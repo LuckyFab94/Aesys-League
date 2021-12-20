e711354b51bf7855f60782ffa6884463c0a0426d
Fabrizio <cerrattifabrizio@gmail.com>
2021-12-20 10:30:39 +0100

    remove footer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@
 import './App.css';
 import Header from './components/Header';
 import React from 'react';
-import { useEffect, useState } from 'react'
-import { getMatch } from './config/action'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Partite from './components/Partite/ListaPartite';
 import ShowTeam from './components/Teams/ShowTeam';
 import ListTeams from './components/Teams/ListTeams';
 import Home from './components/Home/Home';
-import Footer from './components/Footer';
 
 function App() {
   const location = useLocation();
-
-
-  useEffect(() => {
-    //getMatch()
-  }, [])
-
 
 
   return (
@@ -30,7 +21,6 @@ function App() {
         <Route path="/team/:id" element={<ShowTeam />} />
         <Route path="/partite" element={<Partite />} />
       </Routes>
-      <Footer />
     </React.Fragment>
 
   );
